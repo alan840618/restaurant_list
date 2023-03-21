69d1177ee9a15ee662bf8b5ce243f5a99e200763
alan840618 <kb3789@gmail.com>
2023-03-21 21:46:14 +0800

    add category searching function & show restaurant rating on index page

--- app.js
+++ app.js
@@ -4,30 +4,30 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 // set template engine
-app.engine('handlebars',exphbs.engine({defaultLayout:'main'}))
-app.set('view engine','handlebars')
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 // set static file
 app.use(express.static('public'))
 //route setting
 // index page
-app.get('/',(req,res)=>{
-    res.render('index',{restaurants:restaurantList.results})
+app.get('/', (req, res) => {
+    res.render('index', { restaurants: restaurantList.results })
 })
 // show page
-app.get('/restaurants/:restaurant_id',(req,res)=>{
-    const restaurant = restaurantList.results.filter((restaurant)=>{
+app.get('/restaurants/:restaurant_id', (req, res) => {
+    const restaurant = restaurantList.results.filter((restaurant) => {
         return restaurant.id === Number(req.params.restaurant_id)
     })
-    res.render('show',{restaurant:restaurant[0]})
+    res.render('show', { restaurant: restaurant[0] })
 })
 // search
-app.get('/search',(req,res)=>{
+app.get('/search', (req, res) => {
     const keyword = req.query.keyword //查詢字串
-    const filteredRestaurant = restaurantList.results.filter(restaurant=>{
-        return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-    }) 
-    res.render('index',{restaurants:filteredRestaurant,keyword:keyword})
+    const filteredRestaurant = restaurantList.results.filter(restaurant => {
+        return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+    })
+    res.render('index', { restaurants: filteredRestaurant, keyword: keyword })
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Express is listening on localhost:${port}`)
 })
